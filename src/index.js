6e985ff842d4d7e2276b8f97bdfd7a72b2f8a70c
GuilhermeMF <97535820+Lokito2845@users.noreply.github.com>
2024-01-10 02:44:26 +0000

    correção de erros.

--- src/index.js
+++ src/index.js
@@ -17,6 +17,21 @@ var renderPages = function (pageName) {
                     armazenarNome();
                 });
             }
+            if (pageName === "quiz") {
+                carregarPergunta();
+                console.log("A página quiz está pronta para ser usada!");
+            }
+            else if (pageName === "leaderboard") {
+                console.log("A página leaderboard está pronta para ser usada!");
+                var novoJogo = document.querySelector(".btn-gold");
+                // Adicionando um ouvinte de evento ao botão "Novo Jogo"
+                if (novoJogo) {
+                    novoJogo.addEventListener("click", function () {
+                        // Ao clicar no botão "Novo Jogo", retorna à página "start"
+                        renderPages("start");
+                    });
+                }
+            }
         }
         else {
             console.error('Elemento com ID "root" não encontrado.');
@@ -26,25 +41,18 @@ var renderPages = function (pageName) {
         console.error("Erro durante a requisição fetch:", error);
     });
 };
+// Função para exibir mensagens
 renderPages("start");
 function armazenarNome() {
     var nomeUsuarioInput = document.getElementById("input-name");
     if (nomeUsuarioInput && nomeUsuarioInput.value.trim() !== "") {
         var nomeUsuario = nomeUsuarioInput.value;
         localStorage.setItem("usuario", nomeUsuario);
-        var mensagem_1 = criarMensagem("Nome armazenado com sucesso!");
-        document.body.appendChild(mensagem_1);
-        mensagem_1.addEventListener("click", function () {
-            mensagem_1.remove();
-        });
+        console.log("Nome armazenado com sucesso!");
         renderPages("quiz");
     }
     else {
-        var mensagem_2 = criarMensagem("Por favor, digite seu nome antes de começar.");
-        document.body.appendChild(mensagem_2);
-        mensagem_2.addEventListener("click", function () {
-            mensagem_2.remove();
-        });
+        alert("Por favor, digite seu nome antes de começar.");
     }
 }
 function criarMensagem(texto) {
@@ -53,14 +61,9 @@ function criarMensagem(texto) {
     mensagem.classList.add("alert-message");
     return mensagem;
 }
-document.addEventListener("DOMContentLoaded", function () {
-    carregarPergunta();
-});
 function carregarPergunta() {
-    console.log("Iniciando carregarPergunta");
     fetch("questions.json")
         .then(function (resp) {
-        console.log("Requisição fetch concluída");
         if (!resp.ok) {
             throw new Error("Erro na requisição fetch.");
         }
@@ -68,8 +71,68 @@ function carregarPergunta() {
     })
         .then(function (data) {
         console.log("Dados do JSON recebidos:", data);
+        atualizarHTML(data.questions[0]);
     })
         .catch(function (error) {
         console.error("Erro durante a requisição fetch:", error);
     });
+}
+function atualizarHTML(pergunta) {
+    var questionNumberElement = document.querySelector(".question-number");
+    var questionContainerElement = document.querySelector(".question-container");
+    var btnListElement = document.querySelector(".btn-list");
+    var nextBtnElement = document.getElementById("next-btn");
+    if (questionNumberElement && questionContainerElement && btnListElement && nextBtnElement) {
+        questionNumberElement.textContent = "1/10";
+        questionContainerElement.textContent = pergunta.question;
+        btnListElement.innerHTML = "";
+        pergunta.options.forEach(function (option, index) {
+            var button = document.createElement("button");
+            button.className = "btn-answer";
+            button.textContent = option;
+            btnListElement.appendChild(button);
+            button.addEventListener("click", function () {
+                checkAnswer(index);
+                // Remove a cor verde de todos os botões
+                btnListElement.querySelectorAll(".btn-answer").forEach(function (btn) {
+                    var buttonElement = btn;
+                    buttonElement.style.backgroundColor = "";
+                });
+                button.style.backgroundColor = "#B4B7FA";
+            });
+        });
+        nextBtnElement.classList.remove("hide");
+        nextBtnElement.addEventListener("click", function () {
+            if (selectedOptionIndex !== null) {
+                var perguntaAtual = pergunta;
+                verificarResposta(selectedOptionIndex, perguntaAtual.correct);
+            }
+            else {
+                alert("Por favor, selecione uma opção antes de responder.");
+            }
+        });
+    }
+}
+function verificarResposta(selectedIndex, correctIndex) {
+    if (selectedIndex === correctIndex) {
+        alert("Resposta correta!");
+    }
+    else {
+        alert("Resposta incorreta");
+    }
+    renderPages("leaderboard");
+}
+var selectedOptionIndex = null;
+function checkAnswer(index) {
+    if (selectedOptionIndex !== null) {
+        var prevSelectedOption = document.querySelector(".btn-answer[data-index=\"".concat(selectedOptionIndex, "\"]"));
+        if (prevSelectedOption) {
+            prevSelectedOption.style.backgroundColor = "";
+        }
+    }
+    var selectedOption = document.querySelector(".btn-answer[data-index=\"".concat(index, "\"]"));
+    if (selectedOption) {
+        selectedOption.style.backgroundColor = "black";
+    }
+    selectedOptionIndex = index;
 }
--- src/index.ts
+++ src/index.ts
@@ -8,17 +8,36 @@ const renderPages = (pageName: string): void => {
     })
     .then((html) => {
       const rootElem = document.getElementById("root") as HTMLElement;
+
       if (rootElem) {
         rootElem.innerHTML = html;
 
         const formStart = document.getElementById(
           "form-start"
         ) as HTMLFormElement;
+
         if (formStart) {
           formStart.addEventListener("submit", (event) => {
             event.preventDefault();
             armazenarNome();
           });
+        }
+
+        if (pageName === "quiz") {
+          carregarPergunta();
+          console.log("A página quiz está pronta para ser usada!");
+        } else if (pageName === "leaderboard") {
+          
+          console.log("A página leaderboard está pronta para ser usada!");
+          const novoJogo = document.querySelector(".btn-gold");
+
+// Adicionando um ouvinte de evento ao botão "Novo Jogo"
+if (novoJogo) {
+  novoJogo.addEventListener("click", () => {
+    // Ao clicar no botão "Novo Jogo", retorna à página "start"
+    renderPages("start");
+  });
+}
         }
       } else {
         console.error('Elemento com ID "root" não encontrado.');
@@ -29,73 +48,134 @@ const renderPages = (pageName: string): void => {
     });
 };
 
+// Função para exibir mensagens
 renderPages("start");
 
 function armazenarNome(): void {
-  const nomeUsuarioInput = document.getElementById(
-    "input-name"
-  ) as HTMLInputElement | null;
+  const nomeUsuarioInput = document.getElementById("input-name") as HTMLInputElement | null;
 
   if (nomeUsuarioInput && nomeUsuarioInput.value.trim() !== "") {
     const nomeUsuario = nomeUsuarioInput.value;
     localStorage.setItem("usuario", nomeUsuario);
-
-    const mensagem = criarMensagem("Nome armazenado com sucesso!");
-    document.body.appendChild(mensagem);
-    mensagem.addEventListener("click", () => {
-      mensagem.remove();
-    });
+    console.log("Nome armazenado com sucesso!");
     renderPages("quiz");
   } else {
-    const mensagem = criarMensagem(
-      "Por favor, digite seu nome antes de começar."
-    );
-    document.body.appendChild(mensagem);
-
-    mensagem.addEventListener("click", () => {
-      mensagem.remove();
-    });
-  }
+    alert("Por favor, digite seu nome antes de começar.");
+    }
 }
+
 
 function criarMensagem(texto: string): HTMLDivElement {
   const mensagem = document.createElement("div");
+
   mensagem.textContent = texto;
   mensagem.classList.add("alert-message");
+
   return mensagem;
 }
 
-//aparecer perguntas....
-
+function carregarPergunta(): void {
+  fetch("questions.json")
+    .then((resp) => {
+      if (!resp.ok) {
+        throw new Error("Erro na requisição fetch.");
+      }
+      return resp.json();
+    })
+    .then((data) => {
+      console.log("Dados do JSON recebidos:", data);
+      atualizarHTML(data.questions[0]);
+    })
+    .catch((error) => {
+      console.error("Erro durante a requisição fetch:", error);
+    });
+}
 interface Question {
   question: string;
   options: string[];
   correct: number;
 }
-document.addEventListener("DOMContentLoaded", () => {
-  carregarPergunta();
-});
+function atualizarHTML(pergunta: Question): void {
+  const questionNumberElement = document.querySelector(".question-number");
+  const questionContainerElement = document.querySelector(".question-container");
+  const btnListElement = document.querySelector(".btn-list");
+  const nextBtnElement = document.getElementById("next-btn");
 
-function carregarPergunta(): void {
-  console.log("Iniciando carregarPergunta");
-  fetch("questions.json")
-    .then((resp) => {
-      console.log("Requisição fetch concluída");
+  if (questionNumberElement && questionContainerElement && btnListElement && nextBtnElement) {
+    questionNumberElement.textContent = "1/10";
+    questionContainerElement.textContent = pergunta.question;
+    btnListElement.innerHTML = "";
 
-      if (!resp.ok) {
-        throw new Error("Erro na requisição fetch.");
-      }
+    pergunta.options.forEach((option: string, index: number) => {
+      const button = document.createElement("button");
+      button.className = "btn-answer";
+      button.textContent = option;
+      btnListElement.appendChild(button);
 
-      return resp.json();
-    })
-    .then((data) => {
-      console.log("Dados do JSON recebidos:", data);
-    })
-    .catch((error) => {
-      console.error("Erro durante a requisição fetch:", error);
+      button.addEventListener("click", () => {
+        checkAnswer(index);
+
+        // Remove a cor verde de todos os botões
+        btnListElement.querySelectorAll(".btn-answer").forEach((btn) => {
+          const buttonElement = btn as HTMLElement;
+          buttonElement.style.backgroundColor = "";
+        });
+        button.style.backgroundColor = "#B4B7FA";
+        
+      });  
     });
 
-
-
-    
+    nextBtnElement.classList.remove("hide");
+    nextBtnElement.addEventListener("click", () => {
+      if (selectedOptionIndex !== null) {
+        const perguntaAtual = pergunta;
+        verificarResposta(selectedOptionIndex, perguntaAtual.correct);
+      } else {
+        alert("Por favor, selecione uma opção antes de responder.");
+      }
+    });
+  }
 }
+
+
+function verificarResposta(selectedIndex: number, correctIndex: number): void {
+ 
+  if (selectedIndex === correctIndex) {
+    alert("Resposta correta!");
+   
+  } else {
+    alert("Resposta incorreta");
+    
+  }
+
+  renderPages("leaderboard");
+}
+
+let selectedOptionIndex: number | null = null;
+
+
+function checkAnswer(index: number): void {
+  if (selectedOptionIndex !== null) {
+    const prevSelectedOption = document.querySelector(
+      `.btn-answer[data-index="${selectedOptionIndex}"]`
+    ) as HTMLElement | null;
+
+    if (prevSelectedOption) {
+      prevSelectedOption.style.backgroundColor = ""; 
+    }
+  }
+   
+const selectedOption = document.querySelector(
+  `.btn-answer[data-index="${index}"]`
+) as HTMLElement | null;
+
+if (selectedOption) {
+  selectedOption.style.backgroundColor = "black";
+}
+  selectedOptionIndex = index;
+
+
+  
+}
+
+
